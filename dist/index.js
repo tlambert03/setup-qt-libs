19e4ef2d781d81f5f067182e228b54ec90d23b76
Talley Lambert <talley.lambert@gmail.com>
2024-10-11 18:21:10 -0400

    fix #66

--- dist/index.js
+++ dist/index.js
@@ -27872,20 +27872,19 @@ var __webpack_exports__ = {};
 const core = __nccwpck_require__(2186);
 const exec = __nccwpck_require__(1514);
 
-
 // most @actions toolkit packages have async methods
 async function run() {
   try {
     // Qt installer assumes basic requirements that are not installed by
-    // default on Ubuntu.
-    if (process.platform === 'linux') {
-      await exec.exec('sudo apt-get update')
+    // default on Ubuntu. 20.04 and newer only.
+    if (process.platform === "linux") {
+      await exec.exec("sudo apt-get update");
       await exec.exec(
-        'sudo apt-get install -y libegl1 libdbus-1-3 libxkbcommon-x11-0 libxcb-icccm4 libxcb-image0 libxcb-keysyms1 libxcb-randr0 libxcb-render-util0 libxcb-xinerama0 libxcb-xinput0 libxcb-xfixes0 x11-utils libxcb-cursor0 libopengl0 libegl1-mesa'
-      )
+        "sudo apt-get install -y libegl1 libdbus-1-3 libxkbcommon-x11-0 libxcb-icccm4 libxcb-image0 libxcb-keysyms1 libxcb-randr0 libxcb-render-util0 libxcb-xinerama0 libxcb-xinput0 libxcb-xfixes0 x11-utils libxcb-cursor0 libopengl0"
+      );
     }
   } catch (error) {
-    core.setFailed(error.message)
+    core.setFailed(error.message);
   }
 }
 
--- index.js
+++ index.js
@@ -1,20 +1,19 @@
-const core = require('@actions/core');
-const exec = require('@actions/exec');
-
+const core = require("@actions/core");
+const exec = require("@actions/exec");
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
     // Qt installer assumes basic requirements that are not installed by
-    // default on Ubuntu.
-    if (process.platform === 'linux') {
-      await exec.exec('sudo apt-get update')
+    // default on Ubuntu. 20.04 and newer only.
+    if (process.platform === "linux") {
+      await exec.exec("sudo apt-get update");
       await exec.exec(
-        'sudo apt-get install -y libegl1 libdbus-1-3 libxkbcommon-x11-0 libxcb-icccm4 libxcb-image0 libxcb-keysyms1 libxcb-randr0 libxcb-render-util0 libxcb-xinerama0 libxcb-xinput0 libxcb-xfixes0 x11-utils libxcb-cursor0 libopengl0 libegl1-mesa'
-      )
+        "sudo apt-get install -y libegl1 libdbus-1-3 libxkbcommon-x11-0 libxcb-icccm4 libxcb-image0 libxcb-keysyms1 libxcb-randr0 libxcb-render-util0 libxcb-xinerama0 libxcb-xinput0 libxcb-xfixes0 x11-utils libxcb-cursor0 libopengl0"
+      );
     }
   } catch (error) {
-    core.setFailed(error.message)
+    core.setFailed(error.message);
   }
 }
 
